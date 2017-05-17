2eb570b12269d89af8086e2e4f70a19440f9e112
Ben Campbell <ben.in.campbell@gmail.com>
2017-05-17 12:11:27 -0400

    Add LinkedList class and append method

--- linked_list.py
+++ linked_list.py
@@ -7,3 +7,20 @@ class Node(object):
         self.value = value
         self.next = None
 
+
+class LinkedList(object):
+
+    def __init__(self, head=None):
+        self.head = head
+
+
+    def append(self, new_node):
+        """Add a node to the end (or tail) of the linked list"""
+        current = self.head
+        if self.head:
+            while current.next:
+                current = current.next
+            current.next = new_node
+        else:
+            self.head = new_node
+
