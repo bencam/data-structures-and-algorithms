cfbfea9cd9f56ac541742e3a12d257aa827c332f
Ben Campbell <ben.in.campbell@gmail.com>
2017-06-01 14:11:17 -0400

    Add queue.py

---
+++ queue.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+
+class Queue:
+    def __init__(self, head=None):
+        self.storage = [head]
+
+    def enqueue(self, new_element):
+        self.storage.append(new_element)
+
+    def peek(self):
+        return self.storage[0]
+
+    def dequeue(self):
+        return self.storage.pop(0)
+
+    def print_queue(self):
+        for item in self.storage:
+            print item
+
