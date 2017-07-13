613cd6804bce90a013c1f6659e22add9589f9ca0
Ben Campbell <ben.in.campbell@gmail.com>
2017-07-13 10:25:48 -0400

    Add binary_tree.py

---
+++ binary_tree.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+
+class Node(object):
+
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class BinaryTree(object):
+
+    def __init__(self, root):
+        self.root = Node(root)
+
+#<< Add search and print functions >>
+
