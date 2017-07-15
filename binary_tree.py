1b55ca34880001165ea8939d3885fc423d603036
Ben Campbell <ben.in.campbell@gmail.com>
2017-07-15 13:34:25 -0400

    Add search methods

--- binary_tree.py
+++ binary_tree.py
@@ -14,5 +14,18 @@ class BinaryTree(object):
     def __init__(self, root):
         self.root = Node(root)
 
-#<< Add search and print functions >>
+    def search(self, find_val):
+        """Returns True if the specified value is in the tree;
+        returns False otherwise."""
+        return self.preorder_search(self.root, find_val)
+
+    def preorder_search(self, start, find_val):
+        """A helper method used to search recursively."""
+        if start:
+            if start.value == find_val:
+                return True
+            else:
+                return self.preorder_search(start.left, find_val) \
+                    or self.preorder_search(start.right, find_val)
+        return False
 
