aa401ea2d5b64085e798f9cc4c740739eccd0adf
Ben Campbell <ben.in.campbell@gmail.com>
2017-08-03 10:16:48 -0400

    Add start to intro docstring

--- hash_map.py
+++ hash_map.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python
 
 
+"""
+A hash map implementation with one class (HashMap).
+
+The HashMap class consists to five methods:
+
+1. __init__() - creates a list of 10,000 buckets set to a default
+value of None.
+
+2. store() - inserts a string into the hashmap; if the bucket or
+spot where the string is to be inserted already contains a string,
+the string is appended to the bucket. Parameter(s): string.
+
+3. lookup() - 
+
+4. calculate_hash_value() - 
+
+5. get_value() - 
+"""
+
+
 class HashMap(object):
 
 
