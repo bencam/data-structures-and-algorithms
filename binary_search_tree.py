0998bbd8fd64a7bcf567bd03e1de7026394fe8c8
Ben Campbell <ben.in.campbell@gmail.com>
2017-08-24 10:44:35 -0400

    Add binary_search_tree.py

---
+++ binary_search_tree.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+
+"""
+Add docstring
+"""
+
+
+class Node(object):
+
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class BinaryTree(object):
+
+    def __init__(self, root):
+        self.root = Node(root)
+
+    def search():
+
+    def print_tree():
+
+    def preorder_search():
+
+    def preorder_print():
+
