a7866aebb3201d86f6691d6751cef99fa73bfb63
Ben Campbell <ben.in.campbell@gmail.com>
2017-08-24 10:57:47 -0400

    Add append() and print_ll() methods to stack.py

--- stack.py
+++ stack.py
@@ -13,6 +13,28 @@ class LinkedList(object):
     def __init__(self, head=None):
         self.head = head
 
+    def append(self, new_node):
+        """Add a node to the end (or tail) of the linked list"""
+        current = self.head
+        if self.head:
+            while current.next:
+                current = current.next
+            current.next = new_node
+        else:
+            self.head = new_node
+
+    def print_ll(self):
+        current = self.head
+        if current:
+            print 'This is the linked list of nodes:'
+            while current.next:
+                print current.value
+                current = current.next
+            # Prints the last value in the list
+            print current.value
+        else:
+            print 'This is an empty list.'
+
 
 class Stack(object):
 
