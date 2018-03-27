fe1ec44c6d545f9fa2ff02afe053253467bcbe2c
Ben Campbell <ben.in.campbell@gmail.com>
2018-03-27 16:33:46 -0400

    Add note about floor division for python 3

--- binary_search.py
+++ binary_search.py
@@ -9,7 +9,7 @@ def binary_search(input_array, value):
     low = 0
     high = len(input_array) - 1
     while low <= high:
-        mid = (low + high) / 2
+        mid = (low + high) / 2  # See note below
         if input_array[mid] == value:
             return mid
         elif value > input_array[mid]:
@@ -17,3 +17,10 @@ def binary_search(input_array, value):
         else:
             high = mid - 1
     return -1
+
+
+# Note: in Python 3, normal division yields decimals
+(e.g. `7 / 2` returns `3.5`), which would cause problems
+for this algorithm. Floor division (i.e. `//`) in Python 3,
+however, yields whole integers.
+
